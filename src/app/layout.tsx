4acc549646584816cb2f41292bda3fe232381bcc
SAIKAT TANTI <saikat.tanti.fiem.cse23@teamfuture.in>
2024-10-23 19:41:47 +0530

    2nd commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Manrope } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme";
 
 const manrope = Manrope({ subsets: ["latin"] })
 
@@ -22,7 +23,15 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body
-        className={`${manrope.className} bg-[#171717]`}>{children}</body>
+        className={`${manrope.className} bg-[#171717]`}>
+          <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
     </ClerkProvider>
   )
